7eab77d5758aa9136b23ee211dcacbe28565e6a5
kleimj1 <jochen.kleimann@gmail.com>
2025-04-22 23:31:15 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -255,7 +255,7 @@ function main() {
 
     // Handle process termination
     process.on('SIGINT', async () => {
-      await mqttClient.close();
+      await mqttClient.disconnect();
       process.exit();
     });
 
